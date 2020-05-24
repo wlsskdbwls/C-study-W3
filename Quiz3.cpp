59abf60b9f0b825674557bc0e3acc29bdb7ef0c6
wlsskdbwls <63196091+wlsskdbwls@users.noreply.github.com>
2020-05-25 03:12:12 +0900

    Update Quiz3.cpp

--- Quiz3.cpp
+++ Quiz3.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int max(int a, int b) // 매개변수 a,b를 설정한다.
+int max(int a, int b) // 함수 이름은 max로, 매개변수는 정수a,b로 설정한다.
 {
 	if (a > b) // a가 b보다 큰 경우에는
 		return a; // a가 출력
-	else       // 아닌 경우에는 
+	else       // a가 b보다 작은 경우에는 
 		return b; // b가 출력
 }
 
-int min(int a, int b) // 매개변수 a,b를 설정한다.
+int min(int a, int b) // 함수 이름은 min으로, 매개변수는 정수a,b로 설정한다.
 {
 	if (a < b) // a가 b보다 작은 경우에는
 		return a; // a가 출력
-	else      // 아닌 경우에는
+	else      // a가 b보다 큰 경우에는
 		return b; // b가 출력
 }
 
@@ -26,5 +26,5 @@ int main() // 출력은 main함수를 사용
 	cout << endl;
 
 	cout << "큰 수 >>"<< max(a,b) << endl; // max함수를 호출
-	cout << "작은 수 >>"<<min(a,b) << endl; // min함수를 
+	cout << "작은 수 >>"<<min(a,b) << endl; // min함수를 호출 
 }
